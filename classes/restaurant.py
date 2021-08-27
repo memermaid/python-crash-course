1c12dd8a5c438fcbdf651253e6255388f1cc6de1
Natalya <chshebetun@gmail.com>
2021-08-26 20:37:55 -0700

    Created a separate module Restaurant. Created a new file my_restaurant.py to check if the module works.

--- classes/restaurant.py
+++ classes/restaurant.py
@@ -15,24 +15,3 @@ class Restaurant:
 
     def increment_number_served(self, customers_number):
         self.number_served += customers_number
-    
-restaurant1 = Restaurant('Olive Garden', 'american')
-restaurant2 = Restaurant('Bahama Breeze', 'caribbean')
-restaurant3 = Restaurant('Grazie Restorante', 'italian')
-
-restaurant1.set_number_served(10)
-restaurant1.increment_number_served(5)
-restaurant1.describe_restaurant()
-print(f"{restaurant1.restaurant_name} served {restaurant1.number_served} customers today.")
-
-restaurant2.set_number_served(5)
-restaurant2.increment_number_served(7)
-restaurant2.describe_restaurant()
-print(f"{restaurant2.restaurant_name} served {restaurant2.number_served} customers today.")
-
-restaurant3.set_number_served(8)
-restaurant3.increment_number_served(13)
-restaurant3.describe_restaurant()
-print(f"{restaurant3.restaurant_name} served {restaurant3.number_served} customers today.")
-
-
